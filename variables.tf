1244118483d53248b7d9ea7616bb491a4bd97d01
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-06-20 10:37:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,9 @@ variable "secret_key" {
     default = "JCtTygfj2Emak34Fi4LrCUCZbIzLdQjUMJWdh13g"
 }
 
+variable "region" {
+    default = "us-east-1"
+}
 
 variable "instance_type" {
     default = "t2.micro"
